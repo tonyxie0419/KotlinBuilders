10b5ea06edfd39989ddc8777d38234c9e73b6696
tonyxie <tonyxie_0419@qq.com>
2017-09-13 12:10:07 +0800

    function literals with receiver

---
+++ src/FunctionLiteralsWithReceiver.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println(task())
+}
+
+fun task(): List<Boolean> {
+    val isEven: Int.() -> Boolean = { this % 2 == 0 }
+    val isOdd: Int.() -> Boolean = { this % 2 != 0 }
+
+    return listOf(42.isOdd(), 239.isOdd(), 294823098.isEven())
+}
